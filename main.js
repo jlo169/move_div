7df11f2765c17ac01c875986c89c86e463a36aad
Jonathan <kakashi169@gmail.com>
2020-02-27 05:55:13 +0000

    added ability to change color of the square if clicked

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 
 $(document).ready(initializeApp);
 
+const colors = ['red', 'blue', 'yellow', 'green'];
+let colorIndex = 0;
+
 console.log('outside the initialize app')
 
 function initializeApp() {
-  $('.redSquare').on('click', moveDiv);
+  $('.redSquare').on('click', colorDiv);
 }
 
-function moveDiv() {
+function colorDiv() {
+  colorIndex++;
+  if (colorIndex === colors.length) {
+    colorIndex = 0;
+  } 
+
+  $('.redSquare').css('background-color', colors[colorIndex]);
   console.log('pug pug')
 }
