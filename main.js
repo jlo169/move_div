8cc57723b3ed12147449000fdb692297561d6b30
Jonathan <kakashi169@gmail.com>
2020-02-28 07:16:32 +0000

    finished moveDaDiv function, now need to set the interval for checks

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ const colors = ['red', 'blue', 'yellow', 'green'];
 let colorIndex = 0;
 const background = $('.background');
 const square = $('.square');
+const maxValue = background.width() - square.width();
 const keyPressed = {};
 const distance = 3;
 
 function initializeApp() {
-  $('.square').on('click', colorDiv);
+  square.on('click', colorDiv);
   $(window).keydown((sand) => {keyPressed[sand.which] = true});
   $(window).keyup((sand) => {keyPressed[sand.which] = false});
 }
@@ -20,10 +21,13 @@ function colorDiv() {
     colorIndex = 0;
   } 
 
-  $('.square').css('background-color', colors[colorIndex]);
+  square.css('background-color', colors[colorIndex]);
 }
 
 function moveDaDiv(oVal, key1, key2) {
-  const nVal = parseInt(oVal, 10);
+  const nVal = parseInt(oVal, 10)
+    - (keyPressed[key1] ? distance : 0)
+    + (keyPressed[key2] ? distance : 0);
+  return nVal < 0 ? 0 : nVal > maxValue ? maxValue : nVal;
 }
 
