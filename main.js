70e671ab6c9d4989221b44394f99d300a555199e
Jonathan <kakashi169@gmail.com>
2020-02-29 03:45:19 +0000

    added correct borders for both X and Y values

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ $(document).ready(initializeApp);
 
 const colors = ['red', 'blue', 'yellow', 'green'];
 let colorIndex = 0;
-const maxValue = $('.background').width() - $('.square').width();
+let maxValueX;
+let maxValueY;
 const keyPressed = {};
 const distance = 10;
 
@@ -11,21 +12,24 @@ function initializeApp() {
   $('.square').on('click', colorDiv);
   $(window).keydown((sand) => {keyPressed[sand.which] = true});
   $(window).keyup((sand) => {keyPressed[sand.which] = false});
+  maxValueX = $('.background').height() - $('.square').height();
+  maxValueY = $('.background').width() - ($('.square').width());
+  console.log('maxValueX is', maxValueX);
+  console.log('maxValueY is', maxValueY);
 
   setInterval(function() {
     $('.square').css({
       left: (index, oVal) => {
-        return moveDaDiv(oVal, 37, 39);
+        return moveDaDiv(oVal, 37, 39, maxValueY);
       },
       top: (index, oVal) => {
-        return moveDaDiv(oVal, 38, 40);
+        return moveDaDiv(oVal, 38, 40, maxValueX);
       }
     })
   }, 10);
 }
 
 function colorDiv() {
-  console.log('inside colorDiv')
   colorIndex++;
   if (colorIndex === colors.length) {
     colorIndex = 0;
@@ -33,7 +37,7 @@ function colorDiv() {
   $('.square').css('background-color', colors[colorIndex]);
 }
 
-function moveDaDiv(oVal, key1, key2) {
+function moveDaDiv(oVal, key1, key2, maxValue) {
   const nVal = parseInt(oVal, 10)
     - (keyPressed[key1] ? distance : 0)
     + (keyPressed[key2] ? distance : 0);
